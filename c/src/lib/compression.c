5ab4516380f60ec801ea50ed99794a557f092338
Leon Jacobs <leonja511@gmail.com>
2021-01-19 22:52:15 +0200

    (wip) simplify zlib usage by a lot

--- c/src/lib/compression.c
+++ c/src/lib/compression.c
@@ -3,52 +3,51 @@
         https://zlib.net/zpipe.c
 */
 
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <assert.h>
 #include <zlib.h>
 
+#include "debug.h"
 #include "utils.h"
 
-#define CHUNK 1024
-
-int zdepress(char *input, u_int ilen, u_int *olen, char *output)
+char *zdepress(char *input, u_int ilen, u_int *olen)
 {
 
-    int ret;
-    z_stream strm;
-
-    /* allocate inflate state */
-    strm.zalloc = Z_NULL;
-    strm.zfree = Z_NULL;
-    strm.opaque = Z_NULL;
-    strm.avail_in = ilen,
-    strm.next_in = (Bytef *)input;
-    ret = inflateInit(&strm);
-    if (ret != Z_OK)
-        return ret;
-
-    /* 
-        We're not looping like in zpipe as the expectation is to have really small
-        chunks of data to decompress here.
-    */
-
-    strm.avail_out = CHUNK;
-    strm.next_out = (Bytef *) output;
-    ret = inflate(&strm, Z_NO_FLUSH);
+    char *ostream = malloc(ilen);
+    int ret = uncompress((Bytef *)ostream, (uLongf *)olen, (Bytef *)input, ilen + 1);
 
     switch (ret)
     {
-    case Z_NEED_DICT:
-        ret = Z_DATA_ERROR; /* and fall through */
+    case Z_BUF_ERROR:
+        Dprintf(" ! failed to decompress: Z_BUF_ERROR\n");
     case Z_DATA_ERROR:
+        Dprintf(" ! failed to decompress: Z_DATA_ERROR\n");
     case Z_MEM_ERROR:
-        (void)inflateEnd(&strm);
-        return ret;
+        Dprintf(" ! failed to decompress: Z_MEM_ERROR\n");
     }
 
-    *olen = strm.total_out; // write # of decompressed bytes
+    return ostream;
+}
 
-    /* clean up and return */
-    (void)inflateEnd(&strm);
-    return ret == Z_STREAM_END ? Z_OK : Z_DATA_ERROR;
+char *zpress(char *input, u_int ilen, u_int *olen)
+{
+
+    *olen = (u_int)compressBound(ilen);
+    char *ostream = malloc(*olen);
+
+    int res = compress2((Bytef *)ostream, (uLongf *)olen, (Bytef *)input, ilen + 1, Z_BEST_COMPRESSION);
+    if (res == Z_BUF_ERROR)
+    {
+        printf("Buffer was too small!\n");
+        return ostream;
+    }
+    if (res == Z_MEM_ERROR)
+    {
+        printf("Not enough memory for compression!\n");
+        return ostream;
+    }
+
+    return ostream;
 }
--- c/src/lib/compression.h
+++ c/src/lib/compression.h
@@ -1,1 +1,2 @@
-int zdepress(char *input, u_int ilen, u_int *olen, char *output);
+char *zdepress(char *input, u_int ilen, u_int *olen);
+char *zpress(char *input, u_int ilen, u_int *olen);
