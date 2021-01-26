a0624f76a69ba7f33ff8ab9d05920b438e5e820e
Leon Jacobs <leonja511@gmail.com>
2021-01-26 20:15:55 +0200

    (feat) improve debug printing

--- c/godoh/src/util/debug.h
+++ c/godoh/src/util/debug.h
@@ -3,11 +3,12 @@
 
 #include <stdio.h>
 
-// https://stackoverflow.com/a/1941331
-#ifdef DEBUG
-    #define Dprintf(fmt, args...) printf(fmt, ##args)
+// https://stackoverflow.com/a/27351464
+#if defined(DEBUG) && DEBUG > 0
+#define Dprintf(fmt, args...) fprintf(stderr, "DEBUG: ln=%d fn=%s(): " fmt, \
+    __LINE__, __func__, ##args)
 #else
-    #define Dprintf(fmt, args...)
+#define Dprintf(fmt, args...) /* Don't do anything in release builds */
 #endif
 
 #endif //GODOH_DEBUG_H
